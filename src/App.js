9c2c5794e9cc72c84f815f5606cbfd6cb014577b
sum1112 <sumitnitesh123@gmail.com>
2021-11-12 02:20:23 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ const title = 'React';
 function App() {
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>{welcome.greeting} {welcome.title}</h1>
+      <label htmlFor='search'>Search : </label>
+      <input id='search'></input>
     </div>
   );
 }
