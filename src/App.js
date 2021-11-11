fd43386e9ba8d657e388afc5f651b6f9658851f2
sum1112 <sumitnitesh123@gmail.com>
2021-11-12 02:09:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const welcome = {
+  greeting: 'Hey',
+  title: 'React'
+}
+const title = 'React';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello {title}</h1>
+    </div>
+  );
+}
+
+export default App;
