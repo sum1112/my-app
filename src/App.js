44a14e093c550edef0b937286ebd2972a7581544
sum1112 <sumitnitesh123@gmail.com>
2021-11-13 22:41:26 +0530

    Add another component List

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 import React from "react";
 
-const welcome = {
-  greeting: 'Hey',
-  title: 'React'
-}
-const title = 'React';
+const list = [
+  {
+    title : 'React',
+    url : 'https://reactjs.org/',
+    author : 'Jordan Walks',
+    num_comments : 3,
+    points : 4, 
+    objectID : 0,
+  },
+  {
+    title : 'Redux',
+    url : 'https://reactjs.org/',
+    author : 'Dan Abranov, Andrew clark',
+    num_comments : 2,
+    points : 5, 
+    objectID : 1,
+  }
+]
 
 function App() {
   return (
     <div>
-      <h1>{welcome.greeting} {welcome.title}</h1>
+      <h1>My Hacker Stories</h1>
       <label htmlFor='search'>Search : </label>
-      <input id='search'></input>
+      <input id='search' type='text'></input>
+      <hr />
+      <List />
     </div>
-  );
+  )
 }
+
+function List() {
+  return list.map(item => {
+    return (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </div>
+    )
+  });
+}
+
 
 export default App;
