7f3c0e5bd0a77f34f73b7786ec16f2cd1a2da141
sum1112 <sumitnitesh123@gmail.com>
2021-11-13 23:55:15 +0530

    change simple functions to arrow functions

--- src/App.js
+++ src/App.js
@@ -18,33 +18,32 @@ const list = [
     objectID : 1,
   }
 ]
+const Search = () => (
+<div>
+  <label htmlFor='search'>Search : </label>
+  <input id='search' type='text'></input>
+</div>
+);
 
-function App() {
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
-      <label htmlFor='search'>Search : </label>
-      <input id='search' type='text'></input>
-      <hr />
-      <List />
-    </div>
-  )
-}
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
+    <Search />
+    <hr />
+    <List />
+  </div>
+);
 
-function List() {
-  return list.map(item => {
-    return (
-      <div key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </div>
-    )
-  });
-}
+const List = () => list.map(item => (
+  <div key={item.objectID}>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+  </div>
+));
 
 
 export default App;
