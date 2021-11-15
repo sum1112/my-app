2d716132bba8e14e72b0ae71b6ee4bf48d198d63
sum1112 <sumitnitesh123@gmail.com>
2021-11-15 11:39:46 +0530

    Remove list from Global Scope and pass it to List component through props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
 import React from "react";
 
-const list = [
-  {
-    title : 'React',
-    url : 'https://reactjs.org/',
-    author : 'Jordan Walks',
-    num_comments : 3,
-    points : 4, 
-    objectID : 0,
-  },
-  {
-    title : 'Redux',
-    url : 'https://reactjs.org/',
-    author : 'Dan Abranov, Andrew clark',
-    num_comments : 2,
-    points : 5, 
-    objectID : 1,
-  }
-]
 const Search = () => {
+  
   const handleChange = event => {
     console.log(event.target.value);
   }
@@ -29,16 +12,37 @@ const Search = () => {
   );
 }
 
-const App = () => (
-  <div>
-    <h1>My Hacker Stories</h1>
-    <Search />
-    <hr />
-    <List />
-  </div>
-);
+const App = () => {
+  const stories = [
+    {
+      title : 'React',
+      url : 'https://reactjs.org/',
+      author : 'Jordan Walks',
+      num_comments : 3,
+      points : 4, 
+      objectID : 0,
+    },
+    {
+      title : 'Redux',
+      url : 'https://reactjs.org/',
+      author : 'Dan Abranov, Andrew clark',
+      num_comments : 2,
+      points : 5, 
+      objectID : 1,
+    }
+  ]
 
-const List = () => list.map(item => (
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+      <Search />
+      <hr />
+      <List list={stories}/>
+    </div>
+  );
+}
+
+const List = (props) => props.list.map(item => (
   <div key={item.objectID}>
     <span>
       <a href={item.url}>{item.title}</a>
